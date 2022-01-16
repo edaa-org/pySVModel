916982143fc45b9e9cb9e457a73300b5c3dc26a4
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-16 10:50:05 +0100

    Updated file headers.

--- pySVModel/__init__.py
+++ pySVModel/__init__.py
@@ -1,38 +1,37 @@
-# =============================================================================
-#              ____            _              __     __        _ _             __  __           _      _
-#  _ __  _   _/ ___| _   _ ___| |_ ___ _ __ __\ \   / /__ _ __(_) | ___   __ _|  \/  | ___   __| | ___| |
-# | '_ \| | | \___ \| | | / __| __/ _ \ '_ ` _ \ \ / / _ \ '__| | |/ _ \ / _` | |\/| |/ _ \ / _` |/ _ \ |
-# | |_) | |_| |___) | |_| \__ \ ||  __/ | | | | \ V /  __/ |  | | | (_) | (_| | |  | | (_) | (_| |  __/ |
-# | .__/ \__, |____/ \__, |___/\__\___|_| |_| |_|\_/ \___|_|  |_|_|\___/ \__, |_|  |_|\___/ \__,_|\___|_|
-# |_|    |___/       |___/                                               |___/
-# ==============================================================================
-# Authors:            Patrick Lehmann
-#
-# Python package:     An abstract SystemVerilog language model.
-#
-# License:
-# ==============================================================================
-# Copyright 2021-2021 Patrick Lehmann - Boetzingen, Germany
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-# ==============================================================================
+# ==================================================================================================================== #
+#              ______     ____  __           _      _                                                                  #
+#  _ __  _   _/ ___\ \   / /  \/  | ___   __| | ___| |                                                                 #
+# | '_ \| | | \___ \\ \ / /| |\/| |/ _ \ / _` |/ _ \ |                                                                 #
+# | |_) | |_| |___) |\ V / | |  | | (_) | (_| |  __/ |                                                                 #
+# | .__/ \__, |____/  \_/  |_|  |_|\___/ \__,_|\___|_|                                                                 #
+# |_|    |___/                                                                                                         #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2021-2022 Patrick Lehmann - Boetzingen, Germany                                                            #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
 #
 """
 An abstract SystemVerilog language model.
 
-:copyright: Copyright 2021-2021 Patrick Lehmann - Bötzingen, Germany
+:copyright: Copyright 2021-2022 Patrick Lehmann - Bötzingen, Germany
 :license: Apache License, Version 2.0
 """
 from enum import unique, Enum
@@ -43,7 +42,7 @@ from pyTooling.Decorators import export
 
 __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
-__copyright__ = "2021-2021, Patrick Lehmann"
+__copyright__ = "2021-2022, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
 __version__ =   "0.3.5"
 
--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
-# =============================================================================
-#              ______     ____  __           _      _
-#  _ __  _   _/ ___\ \   / /  \/  | ___   __| | ___| |
-# | '_ \| | | \___ \\ \ / /| |\/| |/ _ \ / _` |/ _ \ |
-# | |_) | |_| |___) |\ V / | |  | | (_) | (_| |  __/ |
-# | .__/ \__, |____/  \_/  |_|  |_|\___/ \__,_|\___|_|
-# |_|    |___/
-# ==============================================================================
-# Authors:            Patrick Lehmann
+# ==================================================================================================================== #
+#              ______     ____  __           _      _                                                                  #
+#  _ __  _   _/ ___\ \   / /  \/  | ___   __| | ___| |                                                                 #
+# | '_ \| | | \___ \\ \ / /| |\/| |/ _ \ / _` |/ _ \ |                                                                 #
+# | |_) | |_| |___) |\ V / | |  | | (_) | (_| |  __/ |                                                                 #
+# | .__/ \__, |____/  \_/  |_|  |_|\___/ \__,_|\___|_|                                                                 #
+# |_|    |___/                                                                                                         #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2021-2022 Patrick Lehmann - Boetzingen, Germany                                                            #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
 #
-# Package installer:  An abstract System Verilog language model.
-#
-# License:
-# ============================================================================
-# Copyright 2021-2021 Patrick Lehmann - Boetzingen, Germany
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#		http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
-# ============================================================================
-#
+"""Package installer for 'An abstract System Verilog language model'."""
 from pathlib             import Path
 from pyTooling.Packaging import DescribePythonPackageHostedOnGitHub, DEFAULT_CLASSIFIERS
 
